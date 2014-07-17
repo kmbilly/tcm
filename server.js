9cbb9a9ad81b6f34088efca5b55ae8c4153e95a5
Billy KM LEUNG <bkmleung@ogcio.gov.hk>
2014-07-17 18:06:11 +0800

    Changes to connect to openshift mongodb

--- server.js
+++ server.js
@@ -10,7 +10,15 @@ app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
 app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 
 // Setup db connection pool
-var db = mongojs("mongodb://localhost/tcdb?maxPoolSize=100", ['jtwc_active']);
+var connection_string = 'mongodb://localhost/tcdb?maxPoolSize=100';
+if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+	connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+		process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+		process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+		process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+		process.env.OPENSHIFT_APP_NAME;
+}
+var db = mongojs(connection_string, ['jtwc_active']);
 
 // Setup server shutdown handler
 var shuttingDown = false;
